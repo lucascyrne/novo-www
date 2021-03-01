7da8626ceb521f545fa979daabed881261f44f2a
Lucas Cyrne <lcf@cesar.school>
2021-03-01 18:06:46 -0300

    feat: added header@1.0.0

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,36 +1,40 @@
-import { FunctionalComponent, h } from 'preact';
-import { Link } from 'preact-router/match';
-import style from './style.css';
+import { FunctionalComponent, h } from "preact";
+import { Link } from "preact-router/match";
+import style from "./style.css";
 
 const Header: FunctionalComponent = () => {
-    return (
-        <header id={style.header}>
-            <a id={style.logo} href="/">
-                <img src="" alt="" />
-            </a>
-            <nav>
-                <Link activeClassName={style.active} href="/">
-                    Home
-                </Link>
-                <Link activeClassName={style.active} href="/quem-somos">
-                    Quem somos
-                </Link>
-                <Link activeClassName={style.active} href="/nossas-lojas">
-                    Nossas Lojas
-                </Link>
-                <Link activeClassName={style.active} href="/ofertas">
-                    Ofertas
-                </Link>
-                <Link activeClassName={style.active} href="/contato">
-                    Contato
-                </Link>
-            </nav>
-            <div id={style.location}>
-                <img src="" alt="" />
-                <p>Você está proximo a nossa loja do...</p>
-            </div>
-        </header>
-    );
+  return (
+    <header id={style.header}>
+      <div id={style.logo}>
+        <a href="/">
+          <img src="" alt="" />
+        </a>
+      </div>
+      <div id={style.menu}>
+        <nav>
+          <Link activeClassName={style.active} href="/quem-somos">
+            Quem somos
+          </Link>
+          <Link activeClassName={style.active} href="/nossas-lojas">
+            Nossas Lojas
+          </Link>
+          <Link activeClassName={style.active} href="/ofertas">
+            Ofertas
+          </Link>
+          <Link activeClassName={style.active} href="/cartao-novo">
+            Cartão Novo
+          </Link>
+          <Link activeClassName={style.active} href="/contato">
+            Contato
+          </Link>
+        </nav>
+      </div>
+      <div id={style.location}>
+        <img src="" alt="" />
+        <p>Você está proximo a nossa loja do...</p>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
