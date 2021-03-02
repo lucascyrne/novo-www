5bb7e34a6db0a5741f89133323880335afad2cfd
Lucas Cyrne <lcf@cesar.school>
2021-03-02 18:37:30 -0300

    feat: added noticia-alt@1.0.0 page

---
+++ src/routes/noticias/single/index-alt.tsx
@@ -0,0 +1,44 @@
+import { FunctionalComponent, h } from "preact";
+
+import style from "../style";
+
+const NoticiaSingle: FunctionalComponent = () => {
+  return (
+    <div id={style.noticias}>
+      <h1>Fique por dentro</h1>
+      <div class={style.news_body} id={style.single_body}>
+        <img class={style.news_img} src="" alt="" />
+        <div class={style.social}>
+          Compartilhe:
+          <a href="">
+            <img src="" alt="" />
+          </a>
+          <a href="">
+            <img src="" alt="" />
+          </a>
+          <a href="">
+            <img src="" alt="" />
+          </a>
+          <a href="">
+            <img src="" alt="" />
+          </a>
+        </div>
+        <div class={style.news_info}>
+          <p>Quarta-feira, 10 de dezembro de 2020</p>
+          <h2>Chegamos, Recife!</h2>
+          <p>
+            “Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.”
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NoticiaSingle;
